68ff9e179f6aed1934cc6d1b039c07c0f8b1b8fb
thellost <aldy10ball@gmail.com>
2021-10-07 22:17:35 +0700

    add point movement

---
+++ Problem Solving Project/Assets/Script/Problem5/Problem5.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Problem5 : MonoBehaviour
+{
+    public float speed;
+    Vector2 movement;
+    Rigidbody2D rb;
+    Camera cam;
+    Vector2 point;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        point = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Move(point);
+    }
+
+    public void Move(Vector2 pointPosition)
+    {
+
+        //memakai move toward ke point yang ada di cursor mouse
+        rb.position = Vector3.MoveTowards(transform.position, pointPosition, speed * Time.deltaTime);
+    }
+}
